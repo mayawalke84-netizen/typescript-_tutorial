c3e5e13d6ff897dc42c2b9663204c96fb1b3ee28
mayawalke84-netizen <mayawalke84@gmail.com>
2025-10-29 14:17:28 +0530

    Updated code related to arrays

--- TwoDimensionalArray.js
+++ TwoDimensionalArray.js
@@ -20,3 +20,20 @@ console.log(fruits4[2][0]); // Orange
 var myArray = [[10, "XYZ"], ["ABC", 100]];
 console.log("Printing multi type array........");
 console.log(myArray);
+//access 2D array
+console.log(myArray[0][0]);
+console.log(myArray[0][1]);
+console.log(myArray[1][0]);
+console.log(myArray[1][1]);
+//**********  loop to access array elements using for loop ***********
+for (var i_1 = 0; i_1 < matrix.length; i_1++) {
+    for (var j_1 = 0; j_1 < matrix[i_1].length; j_1++) {
+        console.log(matrix[i_1][j_1]);
+    }
+}
+//**********  loop to access array elements using for in loop ***********
+for (var i in myArray) {
+    for (var j in myArray[i]) {
+        console.log(myArray[i][j]);
+    }
+}
--- TwoDimensionalArray.ts
+++ TwoDimensionalArray.ts
@@ -30,3 +30,28 @@ console.log(fruits4[2][0]); // Orange
 var myArray: (string | number)[][]= [[10, "XYZ"], ["ABC", 100]];
 console.log("Printing multi type array........");
 console.log(myArray);
+
+//access 2D array
+console.log(myArray[0][0]);
+console.log(myArray[0][1]);
+console.log(myArray[1][0]);
+console.log(myArray[1][1]);
+
+
+//**********  loop to access array elements using for loop ***********
+for (let i = 0; i < matrix.length; i++) {
+  for (let j = 0; j < matrix[i].length; j++) {
+    console.log(matrix[i][j]);
+  }
+}
+
+
+//**********  loop to access array elements using for in loop ***********
+
+for (var i in myArray) {
+  for(var j in myArray[i])
+  {
+    console.log(myArray[i][j]);
+  }
+  
+}
